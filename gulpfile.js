7708a99149d0d6fedb175f63df9dfa526cd6c44f
Kostyantin Dumanskui <snotor.dumanskyi.k@gmail.com>
2025-05-22 17:56:57 +0300

    feat: add .prettierrc.js, modify gulpfile and gitignore

--- gulpfile.js
+++ gulpfile.js
@@ -4,13 +4,12 @@ const { task, src, dest } = require('gulp');
 task('build:icons', copyIcons);
 
 function copyIcons() {
-  const version = 'v' + require('./package.json').version.split('.')[0];
-  const nodeSource = path.resolve('nodes', 'Mailtrap', '**', '*.{png,svg}');
-  const nodeDestination = path.resolve('dist');
+  const nodeSource = path.resolve('nodes', '**', '*.{png,svg}');
+  const nodeDestination = path.resolve('dist', 'nodes');
 
   src(nodeSource).pipe(dest(nodeDestination));
 
-  const credSource = path.resolve('nodes', 'Mailtrap', version, 'credentials', '**', '*.{png,svg}');
+  const credSource = path.resolve('credentials', '**', '*.{png,svg}');
   const credDestination = path.resolve('dist', 'credentials');
 
   return src(credSource).pipe(dest(credDestination));
